888d360d0838d98b4c47a636fdec7485fc3dcf47
Dan Kalinin <daniil551@hotmail.com>
2019-01-24 01:04:18 +0300

    Service stubs

---
+++ CoreBluetoothExt/CBEService.h
@@ -0,0 +1,62 @@
+//
+//  CBEService.h
+//  CoreBluetoothExt
+//
+//  Created by Dan Kalinin on 1/23/19.
+//
+
+#import "CBEAttribute.h"
+
+@class CBEService;
+@class CBEServiceOperation;
+
+@protocol CBEServiceDelegate;
+
+
+
+
+
+
+
+
+
+
+@interface CBService (CBE)
+
+@property (readonly) CBEServiceOperation *nseOperation;
+
+@end
+
+
+
+
+
+
+
+
+
+
+@interface CBEService : CBService
+
+@end
+
+
+
+
+
+
+
+
+
+
+@protocol CBEServiceDelegate <CBEAttributeDelegate>
+
+@end
+
+
+
+@interface CBEServiceOperation : CBEAttributeOperation <CBEServiceDelegate>
+
+@property (weak, readonly) CBService *object;
+
+@end
---
+++ CoreBluetoothExt/CBEService.m
@@ -0,0 +1,67 @@
+//
+//  CBEService.m
+//  CoreBluetoothExt
+//
+//  Created by Dan Kalinin on 1/23/19.
+//
+
+#import "CBEService.h"
+
+
+
+
+
+
+
+
+
+
+@implementation CBService (CBE)
+
+@dynamic nseOperation;
+
+- (Class)nseOperationClass {
+    return CBEServiceOperation.class;
+}
+
+@end
+
+
+
+
+
+
+
+
+
+
+@interface CBEService ()
+
+@end
+
+
+
+@implementation CBEService
+
+@end
+
+
+
+
+
+
+
+
+
+
+@interface CBEServiceOperation ()
+
+@end
+
+
+
+@implementation CBEServiceOperation
+
+@dynamic object;
+
+@end
