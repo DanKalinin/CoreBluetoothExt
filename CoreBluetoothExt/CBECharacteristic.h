9dc8d8add61b20b5381e5e4e2f8c532d723bc33a
Dan Kalinin <daniil551@hotmail.com>
2019-01-25 13:53:03 +0300

    Characteristic

---
+++ CoreBluetoothExt/CBECharacteristic.h
@@ -0,0 +1,8 @@
+//
+//  CBECharacteristic.h
+//  CoreBluetoothExt
+//
+//  Created by Dan Kalinin on 1/25/19.
+//
+
+#import "CBEAttribute.h"
---
+++ CoreBluetoothExt/CBECharacteristic.m
@@ -0,0 +1,8 @@
+//
+//  CBECharacteristic.m
+//  CoreBluetoothExt
+//
+//  Created by Dan Kalinin on 1/25/19.
+//
+
+#import "CBECharacteristic.h"
