eca2fc29de57baf88865b55e10d3c703f80db913
Dan Kalinin <daniil551@hotmail.com>
2019-01-31 03:36:54 +0300

    CBEObject

---
+++ CoreBluetoothExt/CBEObject.h
@@ -0,0 +1,37 @@
+//
+//  CBEObject.h
+//  CoreBluetoothExt
+//
+//  Created by Dan Kalinin on 1/31/19.
+//
+
+#import "CBEMain.h"
+
+
+
+
+
+
+
+
+
+
+@interface NSObject (CBE)
+
+@property (readonly) Class cbeOperationClass;
+@property (readonly) NSEObjectOperation *cbeOperation;
+
+@end
+
+
+
+
+
+
+
+
+
+
+@interface CBEObject : NSObject
+
+@end
